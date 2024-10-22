eec58e3aa91fa670560ac0dcde8518a425d1f802
Bartłomiej Bednarek <bartek.bednarek@onet.pl>
2024-10-22 13:17:53 +0200

    popreawa uzytkownik i utworzenie tabela books

---
+++ BibTrans/Migrations/20241022104505_uzytkownik22.10.2024.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BibTrans.Migrations
+{
+    public partial class uzytkownik22102024 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "First_name",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Last_name",
+                table: "AspNetUsers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "First_name",
+                table: "AspNetUsers",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Last_name",
+                table: "AspNetUsers",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
