d61a3ba3895170a37911c34ecd10ab30c792f4dd
Bartłomiej Bednarek <bartek.bednarek@onet.pl>
2024-10-22 23:54:27 +0200

    dodanie nowych tabel

---
+++ BibTrans/Migrations/20241022215355_books_and_borrowing22.10.2024.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BibTrans.Migrations
+{
+    public partial class books_and_borrowing22102024 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
