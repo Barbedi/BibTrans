7a660e0a762423351ed8d5d482c3065189d2e70a
Pawel2923 <pawelporemba123@gmail.com>
2024-10-30 07:22:50 +0100

    Dodaj migracje

---
+++ BibTrans/Migrations/20241030062217_RenameAuthorToAutor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BibTrans.Migrations
+{
+    public partial class RenameAuthorToAutor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Author",
+                table: "Books",
+                newName: "Autor");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Autor",
+                table: "Books",
+                newName: "Author");
+        }
+    }
+}
