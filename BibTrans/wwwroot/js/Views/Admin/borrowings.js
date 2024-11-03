2394806dc81ba156758bea0e42b1c9f38f16bfa6
Pawel2923 <pawelporemba123@gmail.com>
2024-11-03 20:13:08 +0100

    Dodaj układ dla widoku głównego borrowings

---
+++ BibTrans/wwwroot/js/Views/Admin/borrowings.js
@@ -0,0 +1,15 @@
+﻿const searchInput = document.querySelector(".search-input input");
+const searchLabel = document.querySelector(".search-input label");
+
+searchLabel.addEventListener("click", () => {
+    searchInput.focus();
+});
+
+searchInput.addEventListener("input", (ev) => {
+    const searchValueLength = ev.currentTarget.value.length;
+    if (searchValueLength > 0) {
+        searchLabel.classList.add("hidden");
+    } else {
+        searchLabel.classList.remove("hidden");
+    }
+});
