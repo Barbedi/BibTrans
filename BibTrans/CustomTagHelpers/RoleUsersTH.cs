18085b8886b5fa290fe370f2e479480ab5e4d719
Pawel2923 <pawelporemba123@gmail.com>
2024-11-05 12:21:44 +0100

    Aktualizuj RoleUsersTH.cs
    Usuń logger,
    Aktualizuj sposób sprawdzania wartości null dla role i user

--- BibTrans/CustomTagHelpers/RoleUsersTH.cs
+++ BibTrans/CustomTagHelpers/RoleUsersTH.cs
@@ -11,13 +11,11 @@ namespace BibTrans.CustomTagHelpers
     {
         private readonly UserManager<BibTransUser> userManager;
         private readonly RoleManager<IdentityRole> roleManager;
-        private readonly ILogger<RoleUsersTH> logger;
 
         public RoleUsersTH(UserManager<BibTransUser> usermgr, RoleManager<IdentityRole> rolemgr, ILogger<RoleUsersTH> logger)
         {
             userManager = usermgr;
             roleManager = rolemgr;
-            this.logger = logger;
         }
 
         [HtmlAttributeName("i-role")]
@@ -25,25 +23,22 @@ namespace BibTrans.CustomTagHelpers
 
         public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
-            logger.LogInformation($"Processing role: {Role}");
             List<string> names = new();
             IdentityRole role = await roleManager.FindByIdAsync(Role);
-            if (role != null)
+
+            foreach(var user in userManager.Users)
             {
-                logger.LogInformation($"Role found: {role.Name}");
-                foreach (var user in userManager.Users)
+                if (user == null || role == null)
                 {
-                    if (user != null && await userManager.IsInRoleAsync(user, role.Name))
-                    {
-                        names.Add(user.UserName);
-                        logger.LogInformation($"User {user.UserName} is in role {role.Name}");
-                    }
+                    break;
+                }
+
+                if (await userManager.IsInRoleAsync(user, role.Name))
+                {
+                    names.Add(user.UserName);
                 }
             }
-            else
-            {
-                logger.LogWarning($"Role not found: {Role}");
-            }
+
             output.Content.SetContent(names.Count == 0 ? "Brak użytkowników" : string.Join(", ", names));
         }
     }
